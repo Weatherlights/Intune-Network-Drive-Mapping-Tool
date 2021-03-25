d1f6282d6430dbe3f85d1e8912c69c39726b465f
Weatherlights <hauke@hauke.us>
2021-03-25 09:09:09 +0100

    Initial Release

---
+++ Code/Program/DriveSettings.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkDriveMapping
+{
+    public class DriveSettings
+    {
+        private enum ResourceScope
+        {
+            RESOURCE_CONNECTED = 1,
+            RESOURCE_GLOBALNET,
+            RESOURCE_REMEMBERED,
+            RESOURCE_RECENT,
+            RESOURCE_CONTEXT
+        }
+        private enum ResourceType
+        {
+            RESOURCETYPE_ANY,
+            RESOURCETYPE_DISK,
+            RESOURCETYPE_PRINT,
+            RESOURCETYPE_RESERVED
+        }
+        private enum ResourceUsage
+        {
+            RESOURCEUSAGE_CONNECTABLE = 0x00000001,
+            RESOURCEUSAGE_CONTAINER = 0x00000002,
+            RESOURCEUSAGE_NOLOCALDEVICE = 0x00000004,
+            RESOURCEUSAGE_SIBLING = 0x00000008,
+            RESOURCEUSAGE_ATTACHED = 0x00000010
+        }
+        private enum ResourceDisplayType
+        {
+            RESOURCEDISPLAYTYPE_GENERIC,
+            RESOURCEDISPLAYTYPE_DOMAIN,
+            RESOURCEDISPLAYTYPE_SERVER,
+            RESOURCEDISPLAYTYPE_SHARE,
+            RESOURCEDISPLAYTYPE_FILE,
+            RESOURCEDISPLAYTYPE_GROUP,
+            RESOURCEDISPLAYTYPE_NETWORK,
+            RESOURCEDISPLAYTYPE_ROOT,
+            RESOURCEDISPLAYTYPE_SHAREADMIN,
+            RESOURCEDISPLAYTYPE_DIRECTORY,
+            RESOURCEDISPLAYTYPE_TREE,
+            RESOURCEDISPLAYTYPE_NDSCONTAINER
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        private struct NETRESOURCE
+        {
+            public ResourceScope oResourceScope;
+            public ResourceType oResourceType;
+            public ResourceDisplayType oDisplayType;
+            public ResourceUsage oResourceUsage;
+            public string sLocalName;
+            public string sRemoteName;
+            public string sComments;
+            public string sProvider;
+        }
+        [DllImport("mpr.dll")]
+        private static extern int WNetAddConnection2
+            (ref NETRESOURCE oNetworkResource, string sPassword,
+            string sUserName, int iFlags);
+
+        [DllImport("mpr.dll")]
+        private static extern int WNetCancelConnection2
+            (string sLocalName, uint iFlags, int iForce);
+
+        public static void MapNetworkDrive(string sDriveLetter, string sNetworkPath, bool isPersistent)
+        {
+            //Checks if the last character is \ as this causes error on mapping a drive.
+            if (sNetworkPath.Substring(sNetworkPath.Length - 1, 1) == @"\")
+            {
+                sNetworkPath = sNetworkPath.Substring(0, sNetworkPath.Length - 1);
+            }
+
+            NETRESOURCE oNetworkResource = new NETRESOURCE();
+            oNetworkResource.oResourceType = ResourceType.RESOURCETYPE_DISK;
+            oNetworkResource.sLocalName = sDriveLetter + ":";
+            oNetworkResource.sRemoteName = sNetworkPath;
+            if (isPersistent)
+                oNetworkResource.oResourceScope = ResourceScope.RESOURCE_REMEMBERED;
+
+            //If Drive is already mapped disconnect the current 
+            //mapping before adding the new mapping
+            if (IsDriveMapped(sDriveLetter))
+            {
+                DisconnectNetworkDrive(sDriveLetter, true);
+            }
+
+            WNetAddConnection2(ref oNetworkResource, null, null, 0);
+        }
+
+        public static int DisconnectNetworkDrive(string sDriveLetter, bool bForceDisconnect)
+        {
+            if (bForceDisconnect)
+            {
+                return WNetCancelConnection2(sDriveLetter + ":", 0, 1);
+            }
+            else
+            {
+                return WNetCancelConnection2(sDriveLetter + ":", 0, 0);
+            }
+        }
+
+        public static bool IsDriveMapped(string sDriveLetter)
+        {
+            string[] DriveList = Environment.GetLogicalDrives();
+            for (int i = 0; i < DriveList.Length; i++)
+            {
+                if (sDriveLetter + ":\\" == DriveList[i].ToString())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Code/Program/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("NetworkDriveMapping")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("direkt-gruppe GmbH")]
+[assembly: AssemblyProduct("NetworkDriveMapping")]
+[assembly: AssemblyCopyright("Copyright © direkt-gruppe GmbH 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("bbdca7ee-86de-4509-8242-c9381660d290")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
